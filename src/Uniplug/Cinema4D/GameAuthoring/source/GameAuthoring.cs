89e74667f13e3aa73515eeba8cc945c500c6df12
Dominik Steffen <dominik.steffen@googlemail.com>
2015-01-13 17:14:25 +0100

    Some changes and code and stuff.

--- src/Uniplug/Cinema4D/GameAuthoring/source/GameAuthoring.cs
+++ src/Uniplug/Cinema4D/GameAuthoring/source/GameAuthoring.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using C4d;
+using GameAuthoringTools;
 
 
 namespace GameAuthoring
 {
-
-    [CommandPlugin(
-        1000007,
+    // Plugin ID is final from maxon.
+    [C4d.ObjectPlugin(
+        1034424,
         Name = "GameAuthoringPlugin",
-        HelpText = "This plugin can help to develop software with FUSEE in Cinema 4d R16.",
         IconFile = "YourLogoPattern.tif"
         )]
 
-    class FuseeGameAuthoring : CommandData
+    class FuseeGameAuthoring : ObjectDataM
     {
-        public FuseeGameAuthoring() : base(false) { }
+        FuseeAuthoringTools tools;
+
+        public FuseeGameAuthoring() : base(false)
+        {
+            tools = new FuseeAuthoringTools();
+        }
 
         public override bool Execute(BaseDocument doc)
         {
             // Add some functionality here.
             Logger.Debug("GameAuthoring plugin is running.");
+
+            Logger.Debug(tools.RetrieveInformation());
 
             return true;
         }
---
+++ src/Uniplug/Cinema4D/GameAuthoringTools/GameAuthoringTools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAuthoringTools
+{
+    /// <summary>
+    /// The main part for the authoring tools. Registers modules and functionality.
+    /// Can give out references to other tool classes.
+    /// </summary>
+    public class FuseeAuthoringTools
+    {
+        private String s;
+        public FuseeAuthoringTools()
+        {
+            s = "This is a test from the fusee authoring tools set.";
+        }
+
+        public String RetrieveInformation()
+        {
+            return this.s;
+        }
+
+    }
+
+    public class FuseeProjectManager
+    {
+
+    }
+
+    public class FuseeClassManager
+    {
+
+    }
+
+    public class FuseeFileManager
+    {
+
+    }
+
+    public class FuseeBuildManager
+    {
+
+    }
+
+}
