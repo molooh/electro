64e194591092a8924143a26dc5cddb1372db670e
Sarah <sarah.busert@hs-furtwangen.de>
2015-03-11 17:40:42 +0100

    Added TcpServer Example
    TODO: asynchronous change of client message in RenderAFrame

--- src/Engine/Examples/TCP_ServerTest/Main.cs
+++ src/Engine/Examples/TCP_ServerTest/Main.cs
@@ -1,23 +1,60 @@
+using System;
+using System.Runtime.Remoting.Channels;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
 using Fusee.Engine;
 using Fusee.SceneManagement;
 using Fusee.Math;
 
 namespace Examples.TCP_ServerTest
 {
-    public class TCP_ServerTest : RenderCanvas
+
+    internal class TCP_ServerTest : RenderCanvas
     {
+        private GUIText _guiSubText;
+        private GUIText _guiServerText1;
+        private IFont _guiLatoBlack;
+        private GUIHandler _guiHandler;
+
+                
         // is called on startup
         public override void Init()
         {
-            RC.ClearColor = new float4(0.1f, 0.1f, 0.5f, 1);
+            RC.ClearColor = new float4(1, 1, 1, 1);
+
+            Console.WriteLine("Test");
+
+            _guiHandler = new GUIHandler();
+            _guiHandler.AttachToContext(RC);
+
+            _guiLatoBlack = RC.LoadFont("Assets/Lato-Black.ttf", 14);
+            _guiSubText = new GUIText("FUSEE 3D TCP Server Test", _guiLatoBlack, 20, 20);
+            _guiSubText.TextColor = new float4(0, 0, 0, 1f);
+            _guiHandler.Add(_guiSubText);
+
+            
         }
 
         // is called once a frame
         public override void RenderAFrame()
         {
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+            
+            _guiHandler.RenderGUI();
+            string cont = AsynchronousSocketListener.content;
+            if (Input.Instance.IsKeyDown(KeyCodes.W))
+            {
+                Console.WriteLine("Test");
+                //Render(cont);
+            }
 
-            Present();
+           Present();
+            var listener = new AsynchronousSocketListener();
+           listener.Tcp(new string[] { });
         }
 
         // is called when the window was resized
@@ -34,5 +71,13 @@ namespace Examples.TCP_ServerTest
             var app = new TCP_ServerTest();
             app.Run();
         }
+
+        public void Render(string cont)
+        {
+              _guiServerText1.Text = cont;
+              _guiHandler.RenderGUI();
+                     
+        }
     }
+    
 }
