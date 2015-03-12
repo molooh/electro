8cc44979d0591d52f52a3786b70bde22ba267603
Sarah <sarah.busert@hs-furtwangen.de>
2015-03-12 17:19:59 +0100

    Added two parallel threads (fusee/server) to TcpServerExample
    TODO: read string from servers wihle(true)-Loop

---
+++ src/Engine/Examples/TCP_ServerTest/GUI.cs
@@ -0,0 +1,74 @@
+﻿using Fusee.Engine;
+using Fusee.Math;
+
+namespace Examples.PhysicsTest
+{
+    // ReSharper disable once InconsistentNaming
+    class GUI
+    {
+        private readonly GUIHandler _guiHandler;
+
+        private IFont _fontSmall;
+        private IFont _fontMedium;
+        private IFont _fontBig;
+
+        private readonly GUIPanel _guiPanel;
+
+        private GUIText _fps, _serverMsg;
+
+        private readonly float4 _color1 = new float4(1f, 1f, 1f, 1);
+        private readonly float4 _color2 = new float4(0, 0, 0, 1);
+
+        public GUI(RenderContext rc)
+        {
+            //Basic Init
+
+            _fontSmall = rc.LoadFont("Assets/Lato-Black.ttf", 12);
+            _fontMedium = rc.LoadFont("Assets/Lato-Black.ttf", 18);
+            _fontBig = rc.LoadFont("Assets/Lato-Black.ttf", 40);
+
+            _guiHandler = new GUIHandler();
+            _guiHandler.AttachToContext(rc);
+
+            //Start Pannel Init
+            _guiPanel = new GUIPanel("TCP Server Test", _fontMedium, 10, 10, 330, 150);
+
+            _fps = new GUIText("FPS", _fontMedium, 30, 55, _color2);
+            _serverMsg = new GUIText("Message received:", _fontMedium, 30,95,_color2);
+            
+            _guiPanel.ChildElements.Add(_fps);
+            _guiPanel.ChildElements.Add(_serverMsg);
+
+            ShowGUI();
+        }
+
+
+        public void RenderFps(float fps)
+        {
+            _fps.Text = "FPS: " + fps;
+            _guiHandler.RenderGUI();
+        }
+
+        public void RenderMsg(string serverMsg)
+        {
+            _serverMsg.Text = "Message received: " + serverMsg;
+            _guiHandler.RenderGUI();
+        }
+
+
+        
+
+        public void ShowGUI()
+        {
+            _guiHandler.Clear();
+
+            _guiHandler.Add(_guiPanel);
+        }
+
+
+        public void Resize()
+        {
+            _guiHandler.Refresh();
+        }
+    }
+}
--- src/Engine/Examples/TCP_ServerTest/Main.cs
+++ src/Engine/Examples/TCP_ServerTest/Main.cs
@@ -5,7 +5,8 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-
+using System.Threading.Tasks;
+using Examples.PhysicsTest;
 using Fusee.Engine;
 using Fusee.SceneManagement;
 using Fusee.Math;
@@ -16,46 +17,43 @@ namespace Examples.TCP_ServerTest
     internal class TCP_ServerTest : RenderCanvas
     {
         private GUIText _guiSubText;
-        private GUIText _guiServerText1;
+        private GUIText _serverText;
         private IFont _guiLatoBlack;
         private GUIHandler _guiHandler;
+
+        private GUI _gui;
 
                 
         // is called on startup
         public override void Init()
         {
             RC.ClearColor = new float4(1, 1, 1, 1);
-
-            Console.WriteLine("Test");
-
-            _guiHandler = new GUIHandler();
-            _guiHandler.AttachToContext(RC);
-
-            _guiLatoBlack = RC.LoadFont("Assets/Lato-Black.ttf", 14);
-            _guiSubText = new GUIText("FUSEE 3D TCP Server Test", _guiLatoBlack, 20, 20);
-            _guiSubText.TextColor = new float4(0, 0, 0, 1f);
-            _guiHandler.Add(_guiSubText);
-
-            
+            _gui = new GUI(RC);
+           
         }
 
         // is called once a frame
         public override void RenderAFrame()
         {
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
-            
-            _guiHandler.RenderGUI();
-            string cont = AsynchronousSocketListener.content;
-            if (Input.Instance.IsKeyDown(KeyCodes.W))
-            {
-                Console.WriteLine("Test");
-                //Render(cont);
-            }
+            float fps = Time.Instance.FramePerSecond;
+            _gui.RenderFps(fps);
 
+
+            try
+            {
+                var connect = new TcpConnection();
+                //string msg = connect.recvMessage.ToString();
+                _gui.RenderMsg(connect.teststring);
+            }
+            catch(NullReferenceException)
+            {
+                _gui.RenderMsg("Nix da!");
+            }
+            
            Present();
-            var listener = new AsynchronousSocketListener();
-           listener.Tcp(new string[] { });
         }
+
 
         // is called when the window was resized
         public override void Resize()
@@ -66,16 +64,32 @@ namespace Examples.TCP_ServerTest
             RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
         }
 
-        public static void Main()
+
+        public static void StartFusee()
         {
             var app = new TCP_ServerTest();
             app.Run();
         }
 
+        public static void StartTCPServer()
+        {
+            ThreadPoolTcpSrvr tpts = new ThreadPoolTcpSrvr();
+        }
+        
+        
+        public static void Main()
+        {
+            Thread fusee = new Thread(StartFusee);
+            Thread tcpServer = new Thread(StartTCPServer);
+            tcpServer.IsBackground = true;
+
+            tcpServer.Start();
+            fusee.Start();
+        }
+
         public void Render(string cont)
         {
-              _guiServerText1.Text = cont;
-              _guiHandler.RenderGUI();
+        _guiHandler.RenderGUI();
                      
         }
     }
