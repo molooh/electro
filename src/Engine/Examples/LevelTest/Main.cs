5226f4516cc2d30aeaac0122690d7af732846564
anjak610 <anjk610@gmx.de>
2015-05-08 08:15:14 +0200

    Level1_Scene_one

--- src/Engine/Examples/LevelTest/Main.cs
+++ src/Engine/Examples/LevelTest/Main.cs
@@ -1,23 +1,36 @@
 using System;
+using System.IO;
+using System.Linq;
 using Fusee.Engine;
+using Fusee.Engine.SimpleScene;
 using Fusee.Math;
+using Fusee.Serialization;
+
 
 namespace Examples.LevelTest
 {
     public class LevelTest : RenderCanvas
     {
 
-        private static float _angleHorz, _angleVert;
-        private Mesh _worldMesh;
+        private static float _angleHorz, _angleVert, _angleVelHorz, _angleVelVert;
+        private const float RotationSpeed = 1f;
+        private const float Damping = 0.92f;
         
+        private Mesh _worldMesh;
+
+        private float4x4 _modelScaleOffset;
 
        // private ShaderProgram _spColor;
         private ShaderProgram _spTexture;
 
-       // private IShaderParam _colorParam;
-        private IShaderParam _textureParam;
+        private SceneRenderer _sr;
+        private SceneContainer _scene;
+      
 
-        private ITexture _iTex;
+       // private IShaderParam _colorParam;
+        //private IShaderParam _textureParam;
+       
+        //private ITexture _iTex;
 
         // is called on startup
         public override void Init()
@@ -27,36 +40,56 @@ namespace Examples.LevelTest
             _angleHorz = 2;
             _angleVert = 0.2f;
 
-            //_spTexture = RC.CreateShader(Vt, Pt);
-           _worldMesh = MeshReader.LoadMesh(@"Assets/NEW_ISLAND.obj.model");
-           
+          // _spTexture = RC.CreateShader(Vt, Pt);
+          //_worldMesh = MeshReader.LoadMesh(@"Assets/new_Island_finish.obj.model");
+    
+           var ser = new Serializer();
+            using (var file = File.OpenRead(@"Assets/NEW_ISLAND.fus"))
+            {
+                _scene = ser.Deserialize(file, null, typeof(SceneContainer)) as SceneContainer;
+            }
 
-         // _spColor = MoreShaders.GetDiffuseColorShader(RC);
-          // _colorParam = _spColor.GetShaderParam("color");
+            _sr = new SceneRenderer(_scene, "Assets");
+               
+         
+         _spTexture = MoreShaders.GetTextureShader(RC);
+        
+         
+        //  var imgData = RC.LoadImage("Assets/Island.jpg");
+         
 
-           _spTexture = MoreShaders.GetTextureShader(RC);
-           _textureParam = _spTexture.GetShaderParam("texture1");
-
-           var imgData = RC.LoadImage("Assets/Island.jpg");
-          _iTex = RC.CreateTexture(imgData);
-        }
+       }
 
         // is called once a frame
         public override void RenderAFrame()
         {
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
+            if (Input.Instance.IsButton(MouseButtons.Left))
+            {
+                _angleVelHorz = -RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX);
+                _angleVelVert = -RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY);
+            }
+            else
+            {
+                var curDamp = (float)Math.Exp(-Damping * Time.Instance.DeltaTime);
+
+                _angleVelHorz *= curDamp;
+                _angleVelVert *= curDamp;
+            }
+
+            _angleHorz += _angleVelHorz;
+            _angleVert += _angleVelVert;
+
             //TerrainTry
            var mtxRot = float4x4.CreateRotationX(_angleVert) * float4x4.CreateRotationY(_angleHorz);
-           var mtxCam = float4x4.LookAt(1700, 700, 10000, 200, -900, 0, 0,1, 0)*mtxRot;
-           //RC.SetShader(_spColor);
-            RC.SetShader(_spTexture);
+           var mtxCam = float4x4.LookAt(1700, 900, 20000, 200, 0, 0, 0,1, 0)*mtxRot;
+           RC.SetShader(_spTexture);
            RC.ModelView = mtxCam;
-           //RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
-           RC.Render(_worldMesh);
+           _sr.Render(RC);
+         
 
-          
-
+           
             Present();
         }
 
@@ -66,7 +99,7 @@ namespace Examples.LevelTest
             RC.Viewport(0, 0, Width, Height);
 
             var aspectRatio = Width / (float)Height;
-            RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
+            RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 100000);
         }
 
         public static void Main()
