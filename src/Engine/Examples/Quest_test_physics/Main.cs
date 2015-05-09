04ab05d532eb67a4036a51da752f182402963c2e
dropcity <selmueller42@gmail.com>
2015-05-09 22:07:17 +0200

    Physik für einfache Modelle durch bounding boxes

---
+++ src/Engine/Examples/Quest_test_physics/Main.cs
@@ -0,0 +1,287 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Fusee.Engine;
+using Fusee.Engine.SimpleScene;
+using Fusee.Math;
+using Fusee.Serialization;
+
+namespace Examples.Quest_test_physics
+{
+    public class Quest_test_physics : RenderCanvas
+    {
+        // angle variables
+        private static float _angleHorz, _angleVert, _angleVelHorz, _angleVelVert;
+
+        private const float RotationSpeed = 1f;
+        private const float Damping = 0.92f;
+
+        // model variables
+        private Mesh _meshLab;
+        private Mesh _meshBall;
+
+        // variables for shader
+        private ShaderProgram _spColor;
+        private ShaderProgram _spTexture;
+
+        private IShaderParam _colorParam;
+        private IShaderParam _textureParam;
+
+        private ITexture _iTex;
+
+        //Physic
+        private Physic _physic;
+
+        private SceneRenderer _sr;
+        private SceneContainer _scene;
+
+        // is called on startup
+        public override void Init()
+        {
+            RC.ClearColor = new float4(1, 1, 1, 1);
+           
+            var ser = new Serializer();
+            using (var file = File.OpenRead(@"Assets/Sphere.fus"))
+            {
+                var scene = ser.Deserialize(file, null, typeof(SceneContainer)) as SceneContainer;
+                MeshComponent mc = scene.Children.FindComponents<MeshComponent>(comp => true).First();
+                _meshBall = SceneRenderer.MakeMesh(mc);
+            }
+
+
+            using (var file = File.OpenRead(@"Assets/SimpleLab2.fus"))
+            {
+                _scene = ser.Deserialize(file, null, typeof(SceneContainer)) as SceneContainer;
+                //MeshComponent mc = _scene.Children.FindComponents<MeshComponent>(comp => true).First();
+                //_meshLab = SceneRenderer.MakeMesh(mc);
+            }
+
+            _sr = new SceneRenderer(_scene, "Assets");
+             /* 
+            _meshBall = MeshReader.LoadMesh(@"Assets/Sphere.obj.model");
+           _meshLab = MeshReader.LoadMesh(@"Assets/SimpleLab45.obj.model");
+*/
+            _spColor = MoreShaders.GetDiffuseColorShader(RC);
+            _colorParam = _spColor.GetShaderParam("color");
+
+            _physic = new Physic();
+           
+            _physic.InitScene();
+        }
+
+        // is called once a frame
+        public override void RenderAFrame()
+        {
+
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            //var rb2 = _physic.World.GetRigidBody(_physic.World.NumberRigidBodies() - 1);
+            //var rb2 = _physic.World.GetRigidBody(_physic.World.NumberRigidBodies() -5);
+            var rb2 = _physic.GetBall();
+
+            _physic.World.StepSimulation((float)Time.Instance.DeltaTime, (Time.Instance.FramePerSecondSmooth / 60), 1 / 60);//???
+           
+            // move scene per mouse
+            if (Input.Instance.IsButton(MouseButtons.Left))
+            {
+                _angleVelHorz = -RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX);
+                _angleVelVert = -RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY);
+            }
+            else
+            {
+                var curDamp = (float)Math.Exp(-Damping * Time.Instance.DeltaTime);
+
+                _angleVelHorz *= curDamp;
+                _angleVelVert *= curDamp;
+            }
+
+            _angleHorz += _angleVelHorz;
+            _angleVert += _angleVelVert;
+
+
+            // move ball per keyboard
+            if (Input.Instance.IsKeyUp(KeyCodes.Left))
+            {
+                rb2.ApplyCentralImpulse = new float3(-10, 0, 0);
+            }
+
+            //onkeyup
+            if (Input.Instance.IsKeyUp(KeyCodes.Right))
+            {
+                rb2.ApplyCentralImpulse = new float3(10, 0, 0);
+            }
+
+
+            if (Input.Instance.IsKeyUp(KeyCodes.Up))
+            {
+                rb2.ApplyCentralImpulse = new float3(0, 0, 10);
+            }
+
+
+            if (Input.Instance.IsKeyUp(KeyCodes.Down))
+            {
+                rb2.ApplyCentralImpulse = new float3(0, 0, -10);
+            }
+
+            var mtxRot = float4x4.CreateRotationX(_angleVert) * float4x4.CreateRotationY(_angleHorz);
+            //var mtxCam = float4x4.LookAt(0, 200, -500, 0, 0, 0, 0, 1, 0);
+            var mtxCam = float4x4.LookAt(0, 100, -300, 0, 0, 0, 0, 1, 0) * mtxRot;
+
+            RC.SetShader(_spColor);
+
+            if (rb2.CollisionShape is SphereShape)
+            {
+                var shape = (SphereShape)rb2.CollisionShape;
+                var pos = float4x4.Transpose(rb2.WorldTransform);
+                RC.ModelView = mtxCam*pos;// * float4x4.Scale(shape.Radius);
+                RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
+                RC.SetRenderState(new RenderStateSet { AlphaBlendEnable = false, ZEnable = true });
+                RC.Render(_meshBall);
+
+            }
+
+            RC.SetShaderParam(_colorParam, new float4(0.8f, 0.1f, 0.1f, 1));
+            //RC.SetRenderState(new RenderStateSet { AlphaBlendEnable = false, ZEnable = true });
+            RC.ModelView = mtxCam;//* float4x4.CreateTranslation(-100, 0, 0); ;
+            _sr.Render(RC);
+/* 
+            var inner = _physic.GetInnerWall();
+            var matrix = float4x4.Transpose(inner.WorldTransform);
+           Mesh _mesh =  MeshReader.LoadMesh(@"Assets/inner.obj.model");
+           
+                if (inner.CollisionShape is Fusee.Engine.BoxShape)
+                {
+                    var shape = (BoxShape) inner.CollisionShape;
+                    RC.ModelView = mtxCam * matrix * float4x4.Scale(shape.HalfExtents.x / 100, shape.HalfExtents.y / 100, shape.HalfExtents.z / 100);
+
+                    RC.SetShader(_spColor);
+                    RC.SetShaderParam(_colorParam, new float4(0.9f, 0.9f, 0.0f, 1));
+                    RC.SetRenderState(new RenderStateSet {AlphaBlendEnable = false, ZEnable = true});
+                    RC.Render(_mesh);
+                }
+          
+*/             
+/*            
+ * if (rb.CollisionShape is Fusee.Engine.BoxShape)
+                {
+                    var shape = (BoxShape) rb.CollisionShape;
+                    RC.ModelView = mtxCam * matrix * float4x4.Scale(shape.HalfExtents.x / 100, shape.HalfExtents.y / 100, shape.HalfExtents.z / 100);
+     
+                   
+                    RC.SetShader(_spColor);
+                    RC.SetShaderParam(_colorParam, new float4(0.9f, 0.9f, 0.0f, 1));
+                    RC.SetRenderState(new RenderStateSet { AlphaBlendEnable = false, ZEnable = true });
+                    RC.Render(_meshCube);
+                }
+            // render ball mesh
+            for (int i = 0; i < _physic.World.NumberRigidBodies(); i++)
+            {
+                var rb = _physic.World.GetRigidBody(i);
+                var matrix = float4x4.Transpose(rb2.WorldTransform);
+                
+
+                if (rb.CollisionShape is SphereShape)
+                {
+                    var shape = (SphereShape) rb.CollisionShape;
+                    var pos = float4x4.Transpose(rb.WorldTransform);
+                    RC.ModelView = mtxCam*pos*float4x4.Scale(shape.Radius);
+                    RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
+                    RC.SetRenderState(new RenderStateSet {AlphaBlendEnable = false, ZEnable = true});
+                    RC.Render(_meshBall);
+
+                }
+
+                else if (rb.CollisionShape is ConvexHullShape)
+                {
+                    var shape = (ConvexHullShape)rb.CollisionShape;
+                    var pos = float4x4.Transpose(rb.WorldTransform);
+                    RC.ModelView = mtxCam*pos;// * float4x4.CreateRotationX(180); //*mtxRot;// * float4x4.CreateTranslation(150, 0, 0);
+                    RC.SetShaderParam(_colorParam, new float4(0.8f, 0.1f, 0.1f, 1));
+                    RC.SetRenderState(new RenderStateSet {AlphaBlendEnable = false, ZEnable = true});
+                    //RC.Render(_meshLab);
+                }
+                else if (rb.CollisionShape is BoxShape)
+                {
+                    
+                    var shape = (BoxShape)rb.CollisionShape;
+                    var pos = float4x4.Transpose(rb.WorldTransform);
+                    //Debug.WriteLine(pos);
+                    RC.ModelView = mtxCam * pos;// * float4x4.CreateTranslation(wallX[counter], wallY[counter], wallZ[counter]);// * float4x4.CreateRotationX(180); //*mtxRot;// * float4x4.CreateTranslation(150, 0, 0);
+                    RC.SetShaderParam(_colorParam, new float4(0.8f, 0.1f, 0.1f, 1));
+                    RC.SetRenderState(new RenderStateSet { AlphaBlendEnable = false, ZEnable = true });
+                    if (counter == 0)
+                    {
+                        
+                    }
+                    else if (counter > 0 && counter <= 2)
+                    {
+                        RC.Render(_meshWall90);
+                        
+                    }
+                    else if (counter >= 4)
+                    {
+                        RC.Render(_meshWall);
+                        
+                    }
+                    counter++;
+                     
+                }
+
+
+            }
+ */          
+            //ball
+            /*
+            RC.ModelView = mtxCam;
+            RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
+            RC.SetRenderState(new RenderStateSet { AlphaBlendEnable = false, ZEnable = true });
+            RC.Render(_meshBall);
+             * 
+            //walls
+            RC.SetShaderParam(_colorParam, new float4(0.8f, 0.1f, 0.1f, 1));
+            RC.SetRenderState(new RenderStateSet { AlphaBlendEnable = false, ZEnable = true });
+
+            RC.ModelView = mtxCam;//* float4x4.CreateTranslation(-100, 0, 0); ;
+            RC.Render(_meshLab);
+*/
+/*
+            var modelViewMesh1 = mtxCam * mtxRot * float4x4.CreateTranslation(-150, 150, 0) * float4x4.CreateTranslation(0, -50, 0);
+            RC.ModelView = modelViewMesh1;
+            RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
+            RC.Render(_meshBall);
+
+            // render labyrinth mesh
+            var modelViewMesh2 = mtxCam * mtxRot * float4x4.CreateTranslation(150, 0, 0);
+            RC.ModelView = modelViewMesh2;
+            RC.SetShaderParam(_colorParam, new float4(0.5f, 0.1f, 0.5f, 1));
+            RC.Render(_meshLab);
+*/
+            // swap buffers
+            Present();
+        }
+
+        // is called when the window was resized
+        public override void Resize()
+        {
+            RC.Viewport(0, 0, Width, Height);
+
+            var aspectRatio = Width / (float)Height;
+            var projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 5000);
+            RC.Projection = projection;
+        }
+
+        public static void Main()
+        {
+            var app = new Quest_test_physics();
+            app.Run();
+        }
+
+        public override void DeInit()
+        {
+            
+            base.DeInit();
+            _physic.World.Dispose();    //???
+        }
+    }
+}
---
+++ src/Engine/Examples/Quest_test_physics/Physic.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Fusee.Engine;
+using Fusee.Engine.SimpleScene;
+using Fusee.Math;
+using Fusee.Serialization;
+
+
+namespace Examples.Quest_test_physics
+{
+    class Physic
+    {
+        private DynamicWorld _world;
+
+        public DynamicWorld World
+        {
+            get { return _world; }
+            set { _world = value; }
+        }
+
+        internal BoxShape WallCollider90, WallCollider;
+        internal SphereShape MySphereCollider;
+        internal ConvexHullShape MyConvHull;
+
+        private SceneContainer _scene;
+        private IEnumerable<SceneNodeContainer> _sceneList;
+
+        internal Mesh SphereMesh, LabMesh;
+
+        public Physic()
+        {
+            InitScene();
+        }
+
+        public void InitWorld()
+        {
+            _world = new DynamicWorld();
+        }
+        RigidBody inner;
+        public void InitColliders()
+        {
+
+            var ser = new Serializer();
+            using (var file = File.OpenRead(@"Assets/SimpleLab2.fus"))
+            {
+                _scene = ser.Deserialize(file, null, typeof(SceneContainer)) as SceneContainer;
+            }
+            //Island
+            foreach (SceneNodeContainer node in  _scene.Children.FindNodes(node => node.Name.StartsWith("box")))
+            {
+                AABBf? aabb = new AABBCalculator(node).GetBox();
+                float3 boxSize = aabb.Value.Size;
+                float3 boxSizeHalf = boxSize / 2;
+                float3 boxCenter = aabb.Value.Center;
+                float3 min = aabb.Value.min;
+                float3 max = aabb.Value.max;
+
+                
+
+                WallCollider = _world.AddBoxShape(boxSizeHalf.x, boxSizeHalf.y, boxSizeHalf.z);
+                //mass, float3 position, float3 orientation, CollisionShape colShape float3.Zero
+                var lab = _world.AddRigidBody(0, new float3(boxCenter.x, boxCenter.y, boxCenter.z), new float3(0, 0, 0), WallCollider);
+
+                lab.Restitution = 1;
+                lab.Friction = 1;
+                lab.SetDrag(0.0f, 0.05f);
+
+                Debug.WriteLine("#+++#+++#+++#+++#+++#+##+#+#+#+#+##+##+##++rot  " );
+
+            }
+
+            /*
+            foreach (SceneNodeContainer node in _scene.Children.FindNodes(node => node.Name.StartsWith("rot")))
+            {
+               var middle = node.GetMesh();
+                Debug.WriteLine("ROT ######## mesh: " + middle);
+                float3[] verts = middle.Vertices;
+
+                
+                AABBf? aabb = new AABBCalculator(node).GetBox();
+                float3 boxSize = aabb.Value.Size;
+                float3 boxSizeHalf = boxSize / 2;
+                float3 boxCenter = aabb.Value.Center;
+                float3 min = aabb.Value.min;
+                float3 max = aabb.Value.max;
+
+                //rotation modell + Größe ohne bounding box TransformComponent min max aus vertices mesh (aabb calc?)
+                Debug.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++");
+                Debug.WriteLine("min: " + min + " max: " + max + " size: " + boxSizeHalf);
+
+
+                WallCollider = _world.AddBoxShape(boxSizeHalf.x, boxSizeHalf.y, boxSizeHalf.z);
+                //mass, float3 position, float3 orientation, CollisionShape colShape float3.Zero
+                inner = _world.AddRigidBody(0, new float3(boxCenter.x, boxCenter.y, boxCenter.z), new float3(0, 0, 0), WallCollider);
+                
+                inner.Restitution = 1;
+                inner.Friction = 1;
+                inner.SetDrag(0.0f, 0.05f);
+                
+            }
+            //MySphereCollider = _world.AddSphereShape(5);
+            */
+        }
+
+        public void GroundPlane(float3 pos, float3 rot)
+        {
+            var groundShape = _world.AddBoxShape(200, 1, 200);
+            var ground = _world.AddRigidBody(0, pos, rot, groundShape);
+
+            ground.Restitution = 0.5f;
+            ground.Friction = 0.1f;
+        }
+
+        public RigidBody sphere0;
+
+        public void InitScene()
+        {
+            InitWorld();
+            InitColliders();
+            //GroundPlane(float3.Zero, float3.Zero);
+           
+            sphere0 = _world.AddRigidBody(10, new float3(30, 30, 0), float3.Zero, World.AddSphereShape(5));
+            sphere0.Restitution = 0.5f;
+            //sphere0.Friction = 0.8f;
+            //sphere0.CollisionShape = MySphereCollider;
+
+            // sphere.SetDrag(0.0f, 0.05f);
+
+        }
+
+        public RigidBody GetBall()
+        {
+            return sphere0;
+        }
+
+        public RigidBody GetInnerWall()
+        {
+            return inner;
+        }
+
+    }
+
+    
+}
