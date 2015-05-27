b6706893acfa38dc45ceaf5b4fd921c3a436fed5
Dominik Steffen <dominik.steffen@googlemail.com>
2015-05-27 15:39:46 +0200

    last.

--- src/Uniplug/Cinema4D/GameAuthoring/source/GameAuthoring.cs
+++ src/Uniplug/Cinema4D/GameAuthoring/source/GameAuthoring.cs
@@ -7,6 +7,9 @@ using FuseeAuthoringTools.c4dSet;
 
 namespace GameAuthoring.source
 {
+    /// <summary>
+    /// This struct is meant to hold a reference to the engine project during runtime.
+    /// </summary>
     struct PluginReferenceHelper
     {
         private EngineProject _ep;
@@ -20,27 +23,57 @@ namespace GameAuthoring.source
     }
     
     /// <summary>
-    /// This communicates between the plugins.
-    /// TODO: Might not be needed?
+    /// This is meant to communicate between the plugins.
+    /// It can send and receive messages.
     /// </summary>
     [MessagePlugin(ID = 1035161, Name = "MessageHandler", Info = 1 << 29)]
     class FatPluginCommunicator : MessageData
     {
+        /// <summary>
+        /// The timer value in ms.
+        /// So for now it checks for messages after each xx ms.
+        /// </summary>
+        private int _timerValue = 100;
+
+        /// <summary>
+        /// Not used constructor
+        /// </summary>
         public FatPluginCommunicator() : base(false) { }
 
+        /// <summary>
+        /// This is the core message function from c4d.
+        /// It can receive messages from the api.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="bc"></param>
+        /// <returns></returns>
         public override bool CoreMessage(int id, BaseContainer bc)
         {
-            Logger.Debug("MessageHandler ID: " + id);
+            Logger.Debug("MessageHandler ID received: " + id);
+
+            // Now switch for needed ids like C4d.MSG_DOCUMENTINFO_TYPE_SAVE_BEFORE
+
             return false;
         }
 
+        /// <summary>
+        /// This sets the timer interval. It measures how often we check for new messages and is an api function.
+        /// It is measured in ms.
+        /// </summary>
+        /// <returns></returns>
+ 
         public override int GetTimer()
         {
-            return 100;
+            return _timerValue;
         }
     }
 
-    // Plugin ID is final.
+    
+    /// <summary>
+    /// This is the project loader Plugin class.
+    /// It will load a fus project from a given path.
+    /// If the project does not exist, it can create a new one.
+    /// </summary>
     [CommandPlugin(1035056,
        Name = "Fusee Game Authoring Project Helper",
        HelpText = "Opens a Fusee Project and keeps it in memory.'",
@@ -48,7 +81,6 @@ namespace GameAuthoring.source
     ]
     class FatProjectLoaderPlugin : CommandData
     {
-        //Plugin stuff.
         private int PL_PLUGINID = 1035056;
 
         private FuseeAuthoringToolsC4D fat;
@@ -56,6 +88,11 @@ namespace GameAuthoring.source
 
         public FatProjectLoaderPlugin() : base(false) { }
 
+        /// <summary>
+        /// This function is executed when starting the plugin. NOT at initializing.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns></returns>
         public override bool Execute(BaseDocument doc)
         {
             cmdUi = new CmdUiDialog();
@@ -71,7 +108,6 @@ namespace GameAuthoring.source
             String projectName = "TestProjekt";
             String fuseeBinProjPath = "C:/Users/dominik/Development/TestFusee";
 
-            /*
             if (fat.CreateProject(slnName, projectName, fuseeBinProjPath))
             {
                 Logger.Debug("Project opened or created.");
@@ -85,10 +121,17 @@ namespace GameAuthoring.source
             {
                 Logger.Debug("ERROR creating new project!");
             }
-            */
+
             return true;
         }
 
+        /// <summary>
+        /// This function receives messages for a plugin. 
+        /// For now it only prints them out as this plugin type does not use messages in FuseeAT for now.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public override bool Message(int type, SWIGTYPE_p_void data)
         {
             Logger.Debug("Data type from Command Plugin Message: " + data.GetType());
@@ -96,7 +139,11 @@ namespace GameAuthoring.source
         }
     }
 
-    // Plugin ID is final.
+    /// <summary>
+    /// This is the asset tag plugin.
+    /// It can create a tag on different scene objects and do stuff with it.
+    /// It can also react to different messages calls from the api.
+    /// </summary>
     [ TagPlugin(1034424,
         Name = "Fusee Asset Tag",
         IconFile = "icon.tif",
@@ -105,11 +152,18 @@ namespace GameAuthoring.source
         Disklevel = 0) ]
     class FatAssetTagPlugin : TagDataM
     {
-        // private
+        /// <summary>
+        /// This is the reference to FuseeAT.
+        /// </summary>
         private FuseeAuthoringToolsC4D fat;
 
         public FatAssetTagPlugin() : base(false) { }
         
+        /// <summary>
+        /// This is called when the plugin is initialized. So during startup of the application mostly.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public override bool Init(GeListNode node)
         {
             // Creating a connection to the logic behind.
@@ -128,14 +182,28 @@ namespace GameAuthoring.source
             return true;
         }
 
+        /// <summary>
+        /// This reacts to draw calls from the API.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="op"></param>
+        /// <param name="bd"></param>
+        /// <param name="bh"></param>
+        /// <returns></returns>
         public override bool Draw(BaseTag tag, BaseObject op, BaseDraw bd, BaseDrawHelp bh) {
             //Logger.Debug("From Draw()");
             return true;
         }
 
+        /// <summary>
+        /// Reacts to different messages coming from the API and Application.
+        /// Can detect if the scene has been saved etc.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public override bool MessageDocumentInfo(GeListNode node, DocumentInfoData data)
         {
-            // TODO: Check what the message is saying.
             if (data.type == C4dApi.MSG_DOCUMENTINFO_TYPE_SAVE_BEFORE)
             {
                 Logger.Debug("MSG_DOCUMENTINFO_TYPE_SAVE_BEFORE");
@@ -143,7 +211,7 @@ namespace GameAuthoring.source
             else if (data.type == C4dApi.MSG_DOCUMENTINFO_TYPE_SAVE_AFTER)
             {
                 Logger.Debug("MSG_DOCUMENTINFO_TYPE_SAVE_AFTER");
-                // TODO: Now export .fus
+                // Now start the .fus export 
             }
             else if (data.type == C4dApi.MSG_DOCUMENTINFO_TYPE_SAVEPROJECT_BEFORE)
             {
@@ -160,13 +228,12 @@ namespace GameAuthoring.source
 
             return true;
         }
-/*
-        public override bool Message(GeListNode node, int type, SWIGTYPE_p_void data)
-        {
-            Logger.Debug("From Message old.");
-            return false;
-        }
-*/
+
+        /// <summary>
+        /// Little helper function to retrieve the name of a scene object.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         private String GetObjectName(GeListNode node)
         {
             // Call some info.
@@ -179,6 +246,7 @@ namespace GameAuthoring.source
 
     /// <summary>
     /// Handles the GUI functions for the command plugin.
+    /// It can display a form but it cannot react for now due to a very hard api bug due to the wrapping.
     /// </summary>
     public class CmdUiDialog : GeDialog
     {
@@ -192,6 +260,11 @@ namespace GameAuthoring.source
             return true;
         }
 
+        /// <summary>
+        /// This function creates a layout for a dialog window. For now it is a little opening dialog.
+        /// It cannot use decorations like buttons for now because of the bug in the api / wrapping.
+        /// </summary>
+        /// <returns></returns>
         public override bool CreateLayout()
         {
             bool res = base.CreateLayout();
@@ -222,10 +295,15 @@ namespace GameAuthoring.source
             return res;
         }
 
+        /// <summary>
+        /// This function receives messages from the api so we can react to changes in the application.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
         public override int Message(BaseContainer msg, BaseContainer result)
         {
             Logger.Debug("From ui Message.");
-            // TODO: return true if taken care of message, return false if otherwise.
             int msgid = msg.GetId();
 
             return 0;
